7ec20aecc1437a7286e48912464a28dd68dd2717
mittalrr <56380542+mittalrr@users.noreply.github.com>
2020-04-30 20:08:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ os.system('cmd /k "py -m http.server"')
 
 
 ''' 
-1) Place this file in the directory that you want to share
+1) Place this file in the directory that you want to share and then run this file in your command-prompt(terminal)
 2) This would then launch the server @ : http://localhost:8000/
 3) Now to access the contents using some other devices like your PC or Android Phone find the ip address of your laptop or the host system by typing in "ipconfig" in the command prompt.
 4) Then look for the ipv4 address, it will be of the form 192.168.x.x 
